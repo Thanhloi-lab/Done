4feb377deb22b8e58db544e977ce9b3e46058f7a
Loi Cao Thanh <cthanhloi2705@gmail.com>
2022-01-09 20:31:07 +0700

    home job

--- src/components/Job/Home.js
+++ src/components/Job/Home.js
@@ -1,5 +1,5 @@
 import {memo, useState, useEffect} from 'react';
-// import clsx from 'clsx';
+import clsx from 'clsx';
 import { Link } from 'react-router-dom';
 import styles from './page.module.css'
 
@@ -16,13 +16,23 @@ function Home(){
 
     const handleWrap = (id)=>{
         const wrapper = document.getElementById(id);
+        try{
+            if(!wrapper.classList.contains(styles.active)){
+                wrapper.classList.add(styles.active);
+                
+            }
+            else if(wrapper.classList.contains(styles.active)) {
+                const childs = document.querySelectorAll('#' + id + " ." + styles.listProject);
+                childs.forEach((child)=>{
+                    child.classList.remove(styles.active);
+                })
+                wrapper.classList.remove(styles.active);
+            }
+        }
+        catch(e){
+            return
+        }
         
-        if(!wrapper.classList.contains(styles.active)){
-            document.getElementById(id).classList.add(styles.active);
-        }
-        else{
-            document.getElementById(id).classList.remove(styles.active);
-        }
     }
     
     
@@ -30,132 +40,105 @@ function Home(){
         <div className={styles.limiter}>
             <div className={styles.container}>
                 <div className={styles.contentWrapper}>
+                    <div className={styles.contentHeader}>
+                        <h1>MY JOB HOME PAGE</h1>
+                    </div>
+
 
                     <div className={styles.taskContainer}>
-                        <div>
+                        <div className={styles.completed}>
                             <span className={styles.projectHeader}>Completed tasks</span>
                             <span className={styles.wrapBtn} onClick={()=>{
-                                handleWrap('Completed')
+                                handleWrap('completed')
                             }}>
                                 {!wrap.completed ? <i className="fas fa-chevron-down"></i> : <i className="fas fa-chevron-up"></i>}
                             </span>
                         </div>
                         
-                        <ul className={styles.listProject} id="Completed">
+                        <ul className={styles.listProject} id="completed">
                             <li>
-                                <div style={{'fontSize' : '2rem'}}>
+                                <div className={styles.projectContainer}>
                                     <span className={styles.projectTitle}>Project: DONE</span>
                                     <span className={styles.wrapBtn} onClick={()=>{
-                                        // handleWrap('Completed')
+                                         handleWrap('DONE')
                                     }}>
                                         {wrap ? <i className="fas fa-chevron-down"></i> : <i className="fas fa-chevron-up"></i>}
                                     </span>
                                 </div>
-                                <ul>
-                                    <li>
-                                        <p>Xử lý UI</p>
-                                    </li>
-                                </ul>
-                                
-                            </li>
-                            <li>
-                                <ul>
-                                    <li>
-                                        <p>Xử lý backend</p>
+                                <ul className={styles.listProject} id="DONE">
+                                    <li className={styles.projectItem}>
+                                        <p className={styles.taskName}>Xử lý UI</p>
+                                        <div className={styles.comeIcon}>
+                                            <i className="fas fa-chevron-right"></i>
+                                        </div>
                                     </li>
                                 </ul>
                             </li>
                             <li>
-                                <ul>
-                                    <li>
-                                        <p>Xử lý backend</p>
+                                <div className={styles.projectContainer}>
+                                    <span className={styles.projectTitle}>Project: DONE</span>
+                                    <span className={styles.wrapBtn} onClick={()=>{
+                                        //  handleWrap('DONE')
+                                    }}>
+                                        {wrap ? <i className="fas fa-chevron-down"></i> : <i className="fas fa-chevron-up"></i>}
+                                    </span>
+                                </div>
+                                <ul className={styles.listProject+ ' '+ styles.active}>
+                                    <li className={styles.projectItem}>
+                                        <p className={styles.taskName }>Xử lý UI</p>
+                                        <div className={styles.comeIcon}>
+                                            <i className="fas fa-chevron-right"></i>
+                                        </div>
                                     </li>
                                 </ul>
                             </li>
-                            <li>
-                                <ul>
-                                    <li>
-                                        <p>Xử lý backend</p>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <ul>
-                                    <li>
-                                        <p>Xử lý backend</p>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <ul>
-                                    <li>
-                                        <p>Xử lý backend</p>
-                                    </li>
-                                </ul>
-                            </li>
-
                         </ul>
                     </div>
 
                     <div className={styles.taskContainer}>
-                        <div>
+                        <div className={styles.unCompleted}>
                             <span className={styles.projectHeader}>Completed tasks</span>
                             <span className={styles.wrapBtn} onClick={()=>{
-                                handleWrap('uncompleted')
+                                handleWrap('completed')
                             }}>
                                 {!wrap.completed ? <i className="fas fa-chevron-down"></i> : <i className="fas fa-chevron-up"></i>}
                             </span>
                         </div>
                         
-                        <ul className={styles.listProject} id="uncompleted">
+                        <ul className={styles.listProject + ' '+ styles.active} id="completed">
                             <li>
-                                <div style={{'fontSize' : '2rem'}}>
+                                <div className={styles.projectContainer}>
                                     <span className={styles.projectTitle}>Project: DONE</span>
                                     <span className={styles.wrapBtn} onClick={()=>{
-                                        // handleWrap('Completed')
+                                        //  handleWrap('DONE')
                                     }}>
                                         {wrap ? <i className="fas fa-chevron-down"></i> : <i className="fas fa-chevron-up"></i>}
                                     </span>
                                 </div>
-                                <ul>
-                                    <li>
-                                        <p>Xử lý UI</p>
-                                    </li>
-                                </ul>
-                                
-                            </li>
-                            <li>
-                                <ul>
-                                    <li>
-                                        <p>Xử lý backend</p>
+                                <ul className={styles.listProject+ ' '+ styles.active}>
+                                    <li className={styles.projectItem}>
+                                        <p className={styles.taskName}>Xử lý UI</p>
+                                        <div className={styles.comeIconUncomplete}>
+                                            <i className="fas fa-chevron-right"></i>
+                                        </div>
                                     </li>
                                 </ul>
                             </li>
                             <li>
-                                <ul>
-                                    <li>
-                                        <p>Xử lý backend</p>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <ul>
-                                    <li>
-                                        <p>Xử lý backend</p>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <ul>
-                                    <li>
-                                        <p>Xử lý backend</p>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <ul>
-                                    <li>
-                                        <p>Xử lý backend</p>
+                                <div className={styles.projectContainer}>
+                                    <span className={styles.projectTitle}>Project: DONE</span>
+                                    <span className={styles.wrapBtn} onClick={()=>{
+                                        //  handleWrap('DONE')
+                                    }}>
+                                        {wrap ? <i className="fas fa-chevron-down"></i> : <i className="fas fa-chevron-up"></i>}
+                                    </span>
+                                </div>
+                                <ul className={styles.listProject+ ' '+ styles.active}>
+                                    <li className={styles.projectItem}>
+                                        <p className={styles.taskName }>Xử lý UI</p>
+                                        <div className={styles.comeIconUncomplete}>
+                                            <i className="fas fa-chevron-right"></i>
+                                        </div>
                                     </li>
                                 </ul>
                             </li>
